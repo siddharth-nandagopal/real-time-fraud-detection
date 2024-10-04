8c38cd2d359e3244457e8401a303ed6d0313e89e
siddharth nandagopal <snandagopal@ibasis.net>
2024-10-04 12:50:39 -0400

    more comments

--- fraud_api/main.py
+++ fraud_api/main.py
@@ -2,5 +2,7 @@ import uvicorn
 
 from app import app
 
+# Executable programs should define a main function that is called from within a conditional block
+# It starts a server using uvicorn
 if __name__ == '__main__':
     uvicorn.run(app)
